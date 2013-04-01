28fcc7f4d0aa515b83c465b6c1e2db023be71926
charles <couillardcharles@gmail.com>
2013-04-01 22:01:29 +0200

    test coinbase

---
+++ routes/coinbase.js
@@ -0,0 +1,25 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure,
+    MongoClient = require('mongodb').MongoClient;
+
+//var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
+//var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
+
+
+exports.add = function(req, res) {
+    var paiement = req.body;
+    MongoClient.connect("mongodb://coinofglory:bG%)(Ynt7->h[*6@dharma.mongohq.com:10099/coinofglory", function(err, db) {
+        db.collection('paiement', function(err, collection) {
+            collection.insert(paiement, {safe:true}, function(err, result) {
+                if (err) {
+                    res.send({'error':'An error has occurred'});
+                } else {
+                    console.log('Success:');
+                }
+            });
+        });
+    });
+}
