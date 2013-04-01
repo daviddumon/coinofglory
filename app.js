ecefb9c341f0266430f45bbd52f99d57d534c587
charles <couillardcharles@gmail.com>
2013-04-01 17:54:23 +0200

    clean

--- app.js
+++ app.js
@@ -34,17 +34,3 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
-
-
-//var express = require('express');
-//
-//var app = express.createServer(express.logger());
-//
-//app.get('/', function(request, response) {
-//	response.send('Hello World!');
-//    });
-//
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//	console.log("Listening on " + port);
-//    });
