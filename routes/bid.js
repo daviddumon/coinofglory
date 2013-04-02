f1f7bc981d62ce7091e813b8b260e9d5198d55c2
charles <couillardcharles@gmail.com>
2013-04-02 21:25:28 +0200

    test bitpay

--- routes/bid.js
+++ routes/bid.js
@@ -1,4 +1,4 @@
-var bitpay = require('../paiement/bitpay');
+var https = require('https');
 
 
 exports.index = function(req, res){
@@ -6,5 +6,42 @@ exports.index = function(req, res){
 };
 
 exports.creation = function(req, res){
-   bitpay.newInvoice(0.0001);
+   newInvoice(0.0001);
+}
+
+function newInvoice(price) {
+    var newInvoice = { "price" : price, "currency" : "BTC", "posData" : "test-paiement",
+        "notificationURL" : "https://riotous-refuge-9554.herokuapp.com/bitpay-notifications", "fullNotifications":true,
+        "redirectURL" : "http://www.coinofglory.com"};
+    var options = {
+        host: 'bitpay.com',
+        port: 443,
+        path: '/api/invoice/',
+        method: 'POST',
+        auth: 'eE5qBrTa8VPXSr2yGmlDX40FvfxRhmU53ECHfxCrhM:',
+        agent: false
+    };
+
+    var req = https.request(options, function(res) {
+        var data = '';
+        res.on('data', function(chunk) {
+            data += chunk;
+        });
+        res.on('end', function() {
+            var obj;
+            try {
+                obj = JSON.parse(data);
+            } catch(e) {
+                obj = {error: {type: 'parsingError', message: 'Error parsing server response'}};
+            }
+            console.log(obj);
+        });
+    });
+    req.on('error', function(err) {
+        console.error(err);
+    });
+    req.setHeader('Content-Type', 'application/json');
+    var str = JSON.stringify(newInvoice);
+    req.setHeader('Content-Length', str.length);
+    req.end(str);
 }
