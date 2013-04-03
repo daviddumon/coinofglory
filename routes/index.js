48f48762280646458ee52d20278343b6a4c0e17b
jb <bodysplash@gmail.com>
2013-04-03 07:54:15 +0200

    affichage de la dernière image et lien

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,18 @@
+var mongo = require('mongodb');
 
-exports.index = function(req, res){
-  res.render('index', {});
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure,
+    MongoClient = require('mongodb').MongoClient;
+
+
+exports.index = function (req, res) {
+    MongoClient.connect("mongodb://coinofglory:bG%)(Ynt7->h[*6@dharma.mongohq.com:10099/coinofglory", function (err, db) {
+        db.collection('glory', function (err, collection) {
+            collection.find({}, {sort:[['date', 'desc']]}).nextObject(function(err, doc) {
+                console.log(doc);
+                res.render('index', {glory : doc});
+            });
+        });
+    });
 };
