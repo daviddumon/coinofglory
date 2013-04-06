0165d5aae3113c21f1d7823d85b75c57c8979deb
charles <couillardcharles@gmail.com>
2013-04-06 22:10:29 +0200

    on affiche seulement les images payées

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var mongo = require('../mongo/mongofactory');
 exports.index = function (req, res) {
     mongo.execute(function (err, db) {
         db.collection('glory', function (err, collection) {
-            collection.find({}, {sort: [
+            collection.find({paidDate: { $exists: true }}, {sort: [
                 ['paidDate', 'desc']
             ]}).nextObject(function (err, doc) {
                 console.log(doc);
