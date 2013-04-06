198f3871643087b1298d80e10e84cca6fdb46e67
charles <couillardcharles@gmail.com>
2013-04-06 21:28:01 +0200

    affichage sur la home de la dernière gloire payée

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var mongo = require('../mongo/mongofactory');
 exports.index = function (req, res) {
     mongo.execute(function (err, db) {
         db.collection('glory', function (err, collection) {
-            collection.find({}, {sort: [
+            collection.find({paid: true}, {sort: [
                 ['date', 'desc']
             ]}).nextObject(function (err, doc) {
                 console.log(doc);
