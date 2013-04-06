624739f107acb059fa03c2e5f3c8cf4446f63cbb
david <dumon.david@gmail.com>
2013-04-06 12:11:40 +0200

    paiement 0.001BTC

--- routes/bid.js
+++ routes/bid.js
@@ -1,18 +1,24 @@
 var https = require('https');
 
 
-exports.index = function(req, res){
-  res.render('bid', {});
+exports.index = function (req, res) {
+    res.render('bid', {});
 };
 
-exports.creation = function(req, res){
-   newInvoice(0.0002, res);
+exports.creation = function (req, res) {
+    newInvoice(0.001, res);
 }
 
 function newInvoice(price, response) {
-    var newInvoice = { "price" : price, "currency" : "BTC", "posData" : "test-paiement",
-        "notificationURL" : "https://riotous-refuge-9554.herokuapp.com/bitpay-notifications", "fullNotifications":true,
-        "redirectURL" : "http://www.coinofglory.com"};
+
+    var newInvoice = {
+        "price": price,
+        "currency": "BTC",
+        "posData": "test-paiement",
+        "notificationURL": "https://riotous-refuge-9554.herokuapp.com/bitpay-notifications",
+        "fullNotifications": true,
+        "redirectURL": "http://www.coinofglory.com"};
+
     var options = {
         host: 'bitpay.com',
         port: 443,
@@ -22,26 +28,28 @@ function newInvoice(price, response) {
         agent: false
     };
 
-    var req = https.request(options, function(res) {
+    var req = https.request(options, function (res) {
         var data = '';
-        res.on('data', function(chunk) {
+        res.on('data', function (chunk) {
             data += chunk;
         });
-        res.on('end', function() {
+        res.on('end', function () {
             var obj;
             try {
                 obj = JSON.parse(data);
-            } catch(e) {
+            } catch (e) {
                 obj = {error: {type: 'parsingError', message: 'Error parsing server response'}};
             }
             console.log(obj);
-            response.render('bid-ok', {"url" : obj.url});
+            response.render('bid-ok', {"url": obj.url});
         });
     });
-    req.on('error', function(err) {
+
+    req.on('error', function (err) {
         console.error(err);
         response.render('index', {});
     });
+
     req.setHeader('Content-Type', 'application/json');
     var str = JSON.stringify(newInvoice);
     req.setHeader('Content-Length', str.length);
