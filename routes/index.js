0b0017263efc9b0a2de2ad8efcdff3ff5e2ee589
david <dumon.david@gmail.com>
2013-04-07 15:39:34 +0200

    v1 past glories

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ var mongo = require('../mongo/mongofactory'),
 exports.index = function (req, res) {
     var glory_index = parseInt(req.params.index) || 0;
     glory.lastPaid(function (err, doc) {
-        res.render('index', {glory: doc});
+        res.render('index', {glory: doc, past: glory_index + 1});
     }, glory_index);
 };
